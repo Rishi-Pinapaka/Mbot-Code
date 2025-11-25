28d916f1ab72cbf2f382c768cdcf3d86465c57e2
Rishi-Pinapaka <bd6018954@ahschool.com>
2025-11-25 15:29:48 -0500

    Update sketch_nov24a.ino

--- sketch_nov24a/sketch_nov24a.ino
+++ sketch_nov24a/sketch_nov24a.ino
@@ -1,9 +1,32 @@
+#include <Arduino.h>
 #include <MeMCore.h>
 
- 
+ MeDCMotor motor1(M1);
+MeDCMotor motor2(M2);
 
 MeLineFollower lineFinder(PORT_2);
+void moveForward(){
 
+  motor1.run(70);
+  motor2.run(-70);
+
+}
+void turnLeft(){
+  motor1.run(70); 
+  motor2.run(70);
+
+}
+void turnRight(){
+  
+  motor1.run(-70);
+  motor2.run(-70);
+  
+}
+void moveBackward(){
+  motor1.run(-70); 
+  motor2.run(70);
+  
+}
 void setup()
 
 {
@@ -18,26 +41,49 @@ void loop()
 
 {
 
-  int sensorState = lineFinder.readSensors();
+   if (lineFinder.readSensor1()==0 && lineFinder.readSensor2()==0)
 
-  switch(sensorState)
+    {
 
-  {
+      Serial.println("Sensor 1 on black and Sensor2 on black");
+moveForward();
+delay(500);
+    }
 
-    case S1_IN_S2_IN: Serial.println("Sensor 1 and 2 are inside of black line"); break;
+  else if (lineFinder.readSensor1()==1 && lineFinder.readSensor2()==0)
 
-    case S1_IN_S2_OUT: Serial.println("Sensor 2 is outside of black line"); break;
+   {
 
-    case S1_OUT_S2_IN: Serial.println("Sensor 1 is outside of black line"); break;
+     Serial.println("Sensor 1 on white and Sensor 2 on black");
+turnRight();
+delay(500);
+motor1.stop();
+motor2.stop();
+    }
 
-    case S1_OUT_S2_OUT: Serial.println("Sensor 1 and 2 are outside of black line"); break;
+  else if (lineFinder.readSensor1()==0 && lineFinder.readSensor2()==1)
 
-    default: break;
+    {
 
-  }
+    Serial.println("Sensor 1 on Black and Sensor 2 on White");
+turnLeft();
+delay(500);
+motor1.stop();
+motor2.stop();
+    }
 
-  delay(200);
+  else //(lineFinder.readSensor1()==1 && lineFinder.readSensor2()==1)
 
+    {
+
+    Serial.println("Sensor 1 on White and Sensor 2 on White");
+moveBackward();
+delay(500);
+motor1.stop();
+motor2.stop();
+    }
+
+delay(1000);
 }
 
  
